f8658ac7ee53ac73af1a00b4e5bd5807af497df2
ahmed-elnashar <a7medelnashar@hotmail.com>
2020-07-05 14:47:57 +0200

    bubble sort algorithm implementaion

---
+++ sorting/bubble_sort.js
@@ -0,0 +1,15 @@
+let arr = [1, 5, 3, 6, 8, 9, 4, 2];
+
+function bubbleSort(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length; j++) {
+      if (arr[j] > arr[j + 1]) {
+        let temp = arr[j + 1];
+        arr[j + 1] = arr[j];
+        arr[j] = temp;
+      }
+    }
+  }
+
+  return arr;
+}
